081b2292d61c9375fce182c096ed1c6f578da79c
fmpothier <frankiemaepothier@gmail.com>
2024-06-15 14:50:16 -0700

    updated queres so that there is no view being

--- Queries/Warmest_park_weather.sql
+++ Queries/Warmest_park_weather.sql
@@ -1,6 +1,5 @@
-CREATE VIEW Park_Weather_Info AS
-SELECT p.Park_name, ws.avg_summer_temp, ws.avg_summer_rain
+SELECT p.Park_name, ws.avg_summer_temperature, ws.avg_summer_precipitation
 FROM Parks p
-JOIN weather_stations ws ON p.coordinates = ws.coordinates
-ORDER BY ws.avg_summer_temp DESC, ws.avg_summer_rain ASC
+JOIN Weather_stations ws ON p.Location = ws.coordinates
+ORDER BY ws.avg_summer_temperature DESC, ws.avg_summer_precipitation ASC
 LIMIT 1;
--- Queries/coldest_winter_park_weather.sql
+++ Queries/coldest_winter_park_weather.sql
@@ -1,6 +1,5 @@
-CREATE VIEW Park_Winter_Weather_Info AS
 SELECT p.Park_name, ws.avg_winter_temperature, ws.avg_winter_precipitation
 FROM Parks p
-JOIN weather_stations ws ON p.coordinates = ws.coordinates
+JOIN Weather_stations ws ON p.Location = ws.coordinates
 ORDER BY ws.avg_winter_temperature ASC, ws.avg_winter_precipitation DESC
 LIMIT 1;
