f619414c33921439767d6c13b2810f58ee3cf70a
bermuta19 <80367104+bermuta19@users.noreply.github.com>
2024-07-03 15:53:39 -0700

    Update and rename Free_user_view_creation to Free_user_view_creation.sql

--- Queries/Sprint3/Free_user_view_creation.sql
+++ Queries/Sprint3/Free_user_view_creation.sql
@@ -1,7 +1,10 @@
+-- create view for seeing limited access of ratings table
 CREATE VIEW free_ratings AS
 SELECT rating_id,rating_value,comment,park_id
 FROM national_park_ratings
 
+-- create free user User
 CREATE USER 'Free_User'@'%' IDENTIFIED BY 'password';
 
+-- grant them ability to query limited access ratings table
 GRANT SELECT ON free_ratings To Free_User@'%';
